592229b239dc85036bfb45114f1742d39c453766
Tahir Nurullah <nurullahb13@gmail.com>
2025-01-11 08:34:34 +0600

    v1.0.0
    Initial release.
    Version: v1.0.0

--- functions.php
+++ functions.php
@@ -21,3 +21,7 @@ require_once get_parent_theme_file_path( 'inc/admin-features.php' );
 /** Theme block styles and patterns */
 require_once get_parent_theme_file_path( 'inc/blocks/styles/block-styles.php' );
 require_once get_parent_theme_file_path( 'inc/blocks/patterns/block-patterns.php' );
+
+/** Activate Plugins */
+// TN GitHub Theme Updater
+require_once get_parent_theme_file_path( 'inc/activate-plugins/activate-tn-theme-updater.php' );
--- theme-plugins/tn-theme-updater/tn-theme-updater.php
+++ theme-plugins/tn-theme-updater/tn-theme-updater.php
@@ -1,47 +1,94 @@
 <?php
 /*
-Plugin Name: AB Theme Updater
-Description: Handles updates for the Authentic Blog theme.
+Plugin Name: TN GitHub Theme Updater
+Description: Automatically provides updates for the 'authentic-blog' theme from GitHub.
 Version: 1.0
 Author: Tahir Nurullah
 Author URI: https://tahirnurullah.com/
 */
 
-// Check for updates when WordPress loads
-add_filter( 'site_transient_update_themes', 'tn_theme_check_for_update' );
 
-function tn_theme_check_for_update( $transient ) {
-    // Get the current theme version
-    $current_version = wp_get_theme( 'authentic-blog' )->get( 'Version' );
+if ( !class_exists( 'TN_GitHub_Theme_Updater' ) ) {
+    class TN_GitHub_Theme_Updater
+    {
+        private $theme_slug = 'authentic-blog';
+        private $github_user = 'tahirnurullah';
+        private $repo_name = 'authentic-blog';
+        private $github_api_url;
+        private $transient_key;
 
-    // Fetch the update information from your server
-    $response = wp_remote_get( 'https://tahirnurullah.com/tn-updates/theme-update.php?version=' . time() );
+        public function __construct()
+        {
+            $this->github_api_url = "https://api.github.com/repos/{$this->github_user}/{$this->repo_name}/releases";
+            $this->transient_key = "{$this->theme_slug}_update_info";
 
-    if ( is_wp_error( $response ) ) {
-        return $transient; // Return if the request fails
+            add_filter( 'site_transient_update_themes', [$this, 'check_for_updates'] );
+            add_filter( 'upgrader_source_selection', [$this, 'rename_github_folder'], 10, 3 );
+        }
+
+        /**
+         * Check for theme updates from GitHub.
+         */
+        public function check_for_updates( $transient )
+        {
+            if ( empty( $transient->checked ) ) {
+                return $transient;
+            }
+
+            // Use a transient to reduce API calls
+            $release_data = get_transient( $this->transient_key );
+            if ( !$release_data ) {
+                $response = wp_remote_get( $this->github_api_url, [
+                    'headers' => [
+                        'Accept' => 'application/vnd.github.v3+json',
+                    ],
+                ] );
+
+                if ( is_wp_error( $response ) ) {
+                    return $transient;
+                }
+
+                $release_data = json_decode( wp_remote_retrieve_body( $response ), true );
+                if ( empty( $release_data ) ) {
+                    return $transient;
+                }
+
+                set_transient( $this->transient_key, $release_data, HOUR_IN_SECONDS );
+            }
+
+            $latest_release = $release_data[0] ?? null;
+            if ( !$latest_release || empty( $latest_release['tag_name'] ) ) {
+                return $transient;
+            }
+
+            $latest_version = ltrim( $latest_release['tag_name'], 'v' );
+            $current_version = wp_get_theme( $this->theme_slug )->get( 'Version' );
+
+            if ( version_compare( $latest_version, $current_version, '>' ) ) {
+                $transient->response[$this->theme_slug] = [
+                    'new_version' => $latest_version,
+                    'package'     => $latest_release['zipball_url'],
+                    'url'         => $latest_release['html_url'],
+                ];
+            }
+
+            return $transient;
+        }
+
+        /**
+         * Rename the GitHub folder to match the theme slug after download.
+         */
+        public function rename_github_folder( $source, $remote_source, $upgrader )
+        {
+            if ( strpos( $source, $this->repo_name ) !== false ) {
+                $new_source = trailingslashit( $remote_source ) . $this->theme_slug;
+                rename( $source, $new_source );
+                return $new_source;
+            }
+
+            return $source;
+        }
     }
 
-    $update_info = json_decode( wp_remote_retrieve_body( $response ), true ); // Ensure it is an array
-
-    if ( version_compare( $current_version, $update_info['version'], '<' ) ) {
-        // If a new version is available, add update info
-        $transient->response['authentic-blog'] = array(
-            'theme' => 'authentic-blog',
-            'new_version' => $update_info['version'],
-            'url' => $update_info['changelog'],
-            'package' => $update_info['download_url']
-        );
-    }
-
-    return $transient;
-}
-
-// Allow WordPress to handle the update
-add_filter( 'upgrader_source_selection', 'tn_theme_source_selection', 10, 2 );
-function tn_theme_source_selection( $source, $remote ) {
-    if ( isset( $remote['package'] ) && strpos( $remote['package'], 'authentic-blog.zip' ) !== false ) {
-        // Handle the download and installation of the theme
-        return $source;
-    }
-    return $source;
+    new TN_GitHub_Theme_Updater();
 }
